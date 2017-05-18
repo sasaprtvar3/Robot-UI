52216203c7ae54ab7c19ae32deffe08c4b3f003d
roryjackd <roryjackd@yahoo.com>
2017-05-17 19:00:56 -0600

    robot controller functions

---
+++ functions.js
@@ -0,0 +1,19 @@
+function goUp(){
+    console.log(Go Up!);
+}
+
+function goBack(){
+    console.log(Go Back!);
+}
+
+function turnRight(){
+    console.log(Turn Right!);
+}
+
+function turnLeft(){
+    console.log(Turn Left!);
+}
+
+function stop(){
+    console.log(Stop!);
+}
