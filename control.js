1475fa06498b7710245ac91731977ed92fcbe3f3
Elijah McClendon <Elijah@fungry.com>
2017-05-17 20:40:39 -0600

    Adding control.js file

---
+++ control.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Elijah McClendon on 5/17/17.
+ */
+
+export default class Control {
+  constructor(){
+
+  }
+
+  moveBackward(){
+    console.log('moveBackward')
+  }
+
+  moveForward(){
+    console.log('moveForward')
+  }
+
+  stopMovement(){
+    console.log('stopMovement')
+  }
+
+  turnLeft(){
+    console.log('turnLeft')
+  }
+
+  turnRight(){
+    console.log('turnRight')
+  }
+
+}
