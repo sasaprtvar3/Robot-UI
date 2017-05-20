8641cc93553b9a06ec20f1388d7c88e8c68721c4
Saša Prtvar <prtvar11@hotmail.com>
2017-05-19 21:59:06 -0600

    ajax switch

--- functions.js
+++ functions.js
@@ -1,19 +1,57 @@
-function goFoward(){
-    console.log('Go Forward!');
+function ajax(Link) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let x = JSON.parse(this.responseText);
+            console.log(x.A);
+        }
+    };
+    xhttp.open('GET', Link, true);
+    xhttp.send();
 }
 
-function goBack(){
-    console.log('Go Back!');
-}
+function robo(booyah) {
+    console.log(booyah);
+    switch (booyah) {
+        case 'Forward':
+            ajax('/moveBackward');
+            break;
+        case 'Back':
+            ajax('/moveBackward');
+            break;
+        case 'Right':
+            ajax('/moveBackward');
+            break;
+        case 'Left':
+            ajax('/moveBackward');
+            break;
+        case 'Stop':
+            ajax('/moveBackward');
+            break;
+    }
+};
 
-function turnRight(){
-    console.log('Turn Right!');
-}
+// function goFoward() {
+//     ajax('/moveBackward');
+//     console.log('Go Forward!');
+// }
 
-function turnLeft(){
-    console.log('Turn Left!');
-}
+// function goBack() {
+//     ajax('/moveBackward');
+//     console.log('Go Back!');
+// }
 
-function stop(){
-    console.log('Stop!');
-}
+// function turnRight() {
+//     ajax('/moveBackward');
+//     console.log('Turn Right!');
+// }
+
+// function turnLeft() {
+//     ajax('/moveBackward');
+//     console.log('Turn Left!');
+// }
+
+// function stop() {
+//     ajax('/moveBackward');
+//     console.log('Stop!');
+// }
